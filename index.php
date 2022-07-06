9d349a8444c7a7e17aa47c1d34d75c8bd7040ad6
Dmitriy Ignatenko <titarx@gmail.com>
2022-07-06 21:06:44 +0400

    Project commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The main template file
+ *
+ * @link https://workcode.dev
+ *
+ * @package WorkCode
+ * @subpackage Blank Theme
+ * @since Blank Theme 1.0
+ */
+
+get_header();
+?>
+
+Index
+
+<?php
+
+get_footer();
---
+++ singular.php
@@ -0,0 +1,11 @@
+<?php
+
+get_header();
+
+?>
+
+Singular
+
+<?php
+
+get_footer();
